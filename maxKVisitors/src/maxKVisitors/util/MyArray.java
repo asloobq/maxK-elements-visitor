cbbc0196e1d16c378e7afa12f2b3e9a80993c3db
Ganesh Wani <gwani1@binghamton.edu>
2014-11-21 23:21:34 -0500

    Added JavaDoc comments to array and vector

--- maxKVisitors/src/maxKVisitors/util/MyArray.java
+++ maxKVisitors/src/maxKVisitors/util/MyArray.java
@@ -6,19 +6,31 @@ public class MyArray implements Visiting {
 
     private ArrayList<Integer> mArray;
 
+		/*
+		* Constructor
+		*/
     public MyArray() {
        mArray = new ArrayList<Integer>();
        Logger.getInstance().write(Logger.LEVEL.LEVEL_CTOR, getClass().getName() + " ctor");
     }
-
+	
+		/*
+		* @Return Type: void
+		*/
     public void accept(Visitor visitor) {
         visitor.visit(this);
     }
-
+		
+		/*
+		* @Return Type: void
+		*/
    public void add(Integer elem) {
        mArray.add(elem);
    }
 
+		/*
+		* @Return Type: Array list of integers 
+		*/
     public ArrayList<Integer>getmArray(){
         return mArray;
     }
--- maxKVisitors/src/maxKVisitors/util/MyVector.java
+++ maxKVisitors/src/maxKVisitors/util/MyVector.java
@@ -5,20 +5,32 @@ import java.util.Vector;
 public class MyVector implements Visiting {
 
    private Vector<Integer> mVector;
-
+	
+	/**
+	* Constructor
+	*/
    public MyVector() {
        mVector = new Vector<Integer>();
        Logger.getInstance().write(Logger.LEVEL.LEVEL_CTOR, getClass().getName() + " ctor");
    }
-
+	
+	/**
+	* @Return type: void
+	*/
    public void accept(Visitor visitor) {
         visitor.visit(this);
     }
-
+	
+	/**
+	* @Return type: void
+	*/
    public void add(Integer elem) {
         mVector.add(elem);
    }
-
+	
+	/**
+	* @Return type: Vector of integers
+	*/
     public Vector<Integer> getMyVector(){
         return mVector;
     }
